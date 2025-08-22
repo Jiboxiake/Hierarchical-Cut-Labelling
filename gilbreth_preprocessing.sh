801dabf71b767c4a52081b8268d1f76c8c6728ba
Libin <jiboxiake@gmail.com>
2025-08-22 11:51:56 -0400

    experiment on gilbreth

---
+++ gilbreth_preprocessing.sh
@@ -0,0 +1,6 @@
+./index /home/zhou822/roads/W_Beijing/W_Beijing.edges /home/zhou822/roads/W_Beijing/Beijing.hl
+./index /home/zhou822/roads/W_Chengdu/W_Chengdu.edges /home/zhou822/roads/W_Chengdu/Chengdu.hl
+./index /home/zhou822/roads/W_Chicago/W_Chicago.edges /home/zhou822/roads/W_Chicago/Chicago.hl
+./index /home/zhou822/roads/W_Jinan/W_Jinan.edges /home/zhou822/roads/W_Jinan/Jinan.hl
+./index /home/zhou822/roads/W_NewYork/W_NewYork.edges /home/zhou822/roads/W_NewYork/NewYork.hl
+./index /home/zhou822/roads/W_Shenzhen/W_Shenzhen.edges /home/zhou822/roads/W_Shenzhen/Shenzhen.hl
--- src/index.cpp
+++ src/index.cpp
@@ -1,4 +1,5 @@
 #include "road_network.h"
+#include "util.h"
 
 #include <iostream>
 #include <cstring>
@@ -6,7 +7,24 @@
 #include <filesystem>
 using namespace std;
 using namespace road_network;
-
+const size_t MB = 1024 * 1024;
+struct ResultData
+{
+    size_t label_count;
+    size_t max_label_count;
+    size_t index_size;
+    size_t index_height;
+    double index_time;
+    double avg_cut_size;
+    size_t max_cut_size;
+    size_t pruning_2hop;
+    size_t pruning_3hop;
+    size_t pruning_tail;
+    double random_query_time;
+    double random_hoplinks;
+    vector<double> bucket_query_times;
+    vector<double> bucket_hoplinks;
+};
 void preprocess_json(std::string filename,std::string output_name){
     Graph g;
     cout << endl << "reading graph from " << filename << endl;
@@ -30,11 +48,44 @@ void preprocess_json(std::string filename,std::string output_name){
     ContractionIndex con_index(ci, closest);
     // write index
     //json ? con_index.write_json(std::cout) : con_index.write(std::cout);
-    //ofstream index_file;
-    //index_file.open (output_name, ios::out | ios::app );
+    ofstream index_file;
+    index_file.open (output_name, ios::out | ios::app );
     //con_index.write_json(index_file);
-    //con_index.write_json_v2(index_file);
-    //index_file.close();
+    con_index.write_json_v2(index_file);
+    index_file.close();
+}
+
+void preprocess_gilbreth(std::string graph_path,std::string output_name){
+    Graph g;
+    cout << endl << "reading graph from " << graph_path << endl;
+    fstream fs(graph_path.c_str());
+    read_dense_graph(g, fs,true);
+    fs.close();
+    cout << "read " << g.node_count() << " vertices and " << g.edge_count() << " edges" << std::endl;
+
+    util::start_timer();
+    ResultData result = {};
+    vector<Neighbor> closest;
+    g.contract(closest);
+#ifdef NDEBUG
+    srand(time(nullptr));
+    g.randomize();
+#endif
+    // construct index
+    vector<CutIndex> ci;
+    g.create_cut_index(ci, 0.2);
+    ContractionIndex con_index(ci, closest);
+    result.index_time = util::stop_timer();
+    result.index_size = con_index.size() / MB;
+    std::cout<<"index build"<<std::endl;
+    cout << "created index of size " << result.index_size << " MB in " << result.index_time << "s" << endl;
+    // write index
+    //json ? con_index.write_json(std::cout) : con_index.write(std::cout);
+    ofstream index_file;
+    index_file.open (output_name, ios::out | ios::app );
+    //con_index.write_json(index_file);
+    con_index.write_json_v2(index_file);
+    index_file.close();
 }
 
 void compute_ground_truth(std::string graph_path, std::string query_path,std::string output_name){
@@ -90,9 +141,15 @@ void compute_ground_truth(std::string graph_path, std::string query_path,std::st
 int main(int argc, char** argv)
 {
     std::cout<<"run index"<<std::endl;
+    if(argc<3){
+        cerr << "Usage: " << argv[0] << " <graph_file> <output_file>" << endl;
+        return 1;
+    }
+    preprocess_gilbreth(argv[1], argv[2]);
+    //preprocess_gilbreth("/home/zhou822/roads/W_Beijing/W_Beijing.edges","/home/zhou822/roads/W_Beijing/Beijing.hl");
     //compute_ground_truth("/scratch1/zhou822/Beijing_Cleaned/adjacency_list.txt","/scratch1/zhou822/Beijing_Cleaned/queries.txt","/scratch1/zhou822/Beijing_Cleaned/ground_truth.txt");
     //for new york
-    compute_ground_truth("/scratch1/zhou822/NewYork_v1/NewYork.edgelist","/scratch1/zhou822/NewYork_v1/NewYork.queries","/scratch1/zhou822/NewYork_v1/NewYork.groundtruth");
+    //compute_ground_truth("/scratch1/zhou822/NewYork_v1/NewYork.edgelist","/scratch1/zhou822/NewYork_v1/NewYork.queries","/scratch1/zhou822/NewYork_v1/NewYork.groundtruth");
     return 0;
     /*bool json = false;
     if (argc > 1)
